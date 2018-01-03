5cf8b477be69d4b288ac81063974d1505a383370
Alex Mooney <AlexMMooney@gmail.com>
2018-01-03 08:51:22 -0500

    Separate MC data from inputs

--- blood_hunt.js
+++ blood_hunt.js
@@ -1,27 +1,26 @@
-var app = new Vue({
-  el: '#app',
+let inputs = new Vue({
+  el: '#inputs',
   data: {
+    mc_rounds: 1,
+    months: 12,
     hunters: 1,
     blood_level: 1,
     upkeep: 55,
     starting_pop: 5500,
     order: 0,
-    growth: 0,
-    months: [
-      {
-        number: 1,
-        population: 0,
-        unrest: 0,
-        income: 0,
-        slaves: 0,
-        lost_income: 0,
-        lost_taxes: 0,
-        upkeep: 0,
-        total_gold: 0,
-        cost: 0
-      },
-    ]
+    growth: 0
+  },
+  updated: function () {
+    monte_carlo()
   }
 });
 
-console.log('loaded blood_hunt.js');
+let results = new Vue({
+  el: '#results',
+  data: {
+    months: []
+  }
+});
+
+function monte_carlo() {
+}
